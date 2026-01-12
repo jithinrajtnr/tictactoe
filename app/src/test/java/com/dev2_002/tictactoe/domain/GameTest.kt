2d440342b73a554b3457c7144375b2c9949f484d
2026-DEV2-002 <2026-DEV2-002@users.noreply.github.com>
2026-01-12 19:57:57 +0100

    chore: add test structure for domain layer
    - Create domain test directory
    - Add GameTest placeholder class
    - Verify test setup works

---
+++ app/src/test/java/com/dev2_002/tictactoe/domain/GameTest.kt
@@ -0,0 +1,16 @@
+package com.dev2_002.tictactoe.domain
+
+import org.junit.Test
+
+/**
+ * Unit tests for TicTacToe game logic.
+ * Following TDD approach - tests written first.
+ */
+class GameTest {
+
+    @Test
+    fun `placeholder test to verify test setup works`() {
+        // This placeholder will be replaced with actual tests
+        assert(true)
+    }
+}
