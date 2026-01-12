e83fe5637ed329534a93aee2e9caaaf10e377d94
2026-DEV2-002 <2026-DEV2-002@users.noreply.github.com>
2026-01-12 20:01:47 +0100

    new game should start with player X , first move should be made by player X

--- app/src/test/java/com/dev2_002/tictactoe/domain/GameTest.kt
+++ app/src/test/java/com/dev2_002/tictactoe/domain/GameTest.kt
@@ -1,16 +1,24 @@
 package com.dev2_002.tictactoe.domain
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 /**
  * Unit tests for TicTacToe game logic.
- * Following TDD approach - tests written first.
  */
 class GameTest {
 
+    // Rule 1: X always goes first
+
     @Test
-    fun `placeholder test to verify test setup works`() {
-        // This placeholder will be replaced with actual tests
-        assert(true)
+    fun `new game should start with player X`() {
+        val game = Game()
+        assertEquals(Player.X, game.currentPlayer)
+    }
+
+    @Test
+    fun `first move should be made by player X`() {
+        val game = Game()
+        assertEquals(Player.X, game.currentPlayer)
     }
 }
