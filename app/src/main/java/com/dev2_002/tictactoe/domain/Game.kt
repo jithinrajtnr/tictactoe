b8f3fdcf6d34a426cdd5db082995797f7b19ef1f
2026-DEV2-002 <2026-DEV2-002@users.noreply.github.com>
2026-01-12 20:06:27 +0100

    feat: implement X goes first rule

---
+++ app/src/main/java/com/dev2_002/tictactoe/domain/Game.kt
@@ -0,0 +1,5 @@
+package com.dev2_002.tictactoe.domain
+
+class Game {
+    val currentPlayer: Player = Player.X
+}
---
+++ app/src/main/java/com/dev2_002/tictactoe/domain/Player.kt
@@ -0,0 +1,5 @@
+package com.dev2_002.tictactoe.domain
+
+enum class Player {
+    X, O
+}
